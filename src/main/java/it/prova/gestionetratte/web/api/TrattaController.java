8691df8d40d6c11dac63743a19e49d8217f004fb
System Administrator <root@MacBook-Air-di-Alessandro.local>
2022-11-26 09:32:41 +0100

    implementazione classi DTO Airbus-Tratta

--- src/main/java/it/prova/gestionetratte/web/api/TrattaController.java
+++ src/main/java/it/prova/gestionetratte/web/api/TrattaController.java
@@ -1,5 +1,16 @@
 package it.prova.gestionetratte.web.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.prova.gestionetratte.service.TrattaService;
+
+@RestController
+@RequestMapping("api/tratta")
 public class TrattaController {
+	
+	@Autowired
+	private TrattaService trattaService;
 
 }
